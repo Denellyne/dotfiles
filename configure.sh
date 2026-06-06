9337a6f33ddf308308ae38be91d7d7cb28817a47
Gustavo Barbosa Vieira dos Santos <gustavobarbosavs@gmail.com>
2026-06-06 22:37:58 +0100

    I forgot to update configure.sh oops

--- configure.sh
+++ configure.sh
@@ -9,8 +9,13 @@ cp ./.tmux.conf ~/
 tmux source-file ~/.tmux.conf > /dev/null 2>&1
 
 echo "Copying Hyprland config..."
-cp -r ./hypr/ ./waybar/ ~/.config/
+cp -r ./mako/ ./cava/ ./hypr/ ./waybar/ ~/.config/
 hyprctl reload > /dev/null 2>&1
+makoctl reload > /dev/null 2>&1
+pkill cava > /dev/null 2>&1
+pkill cava.sh > /dev/null 2>&1
+pkill waybar > /dev/null 2>&1
+hyprctl dispatch exec waybar > /dev/null 2>&1
 
 echo "Copying nvim config..."
 cp -r ./nvim/ ~/.config/
